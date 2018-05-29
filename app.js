f9a2d85e4794fbd187c38d6bf10c98bfca27a21c
Ippei SUZUKI <i.suzuki@jiec.co.jp>
2018-05-29 13:49:23 +0900

    ファイルアップロード作成

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const
     cfenv = require('cfenv'),
     express = require('express'),
     favicon = require('serve-favicon'),
+    fs = require('fs'),
     morgan = require('morgan'),
+    multer = require('multer'),
     path = require('path'),
     AWS = require('ibm-cos-sdk');
 
@@ -18,9 +20,6 @@ const
 const
     app = express(),
     appEnv = cfenv.getAppEnv();
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 // サービス資格情報を取得する。
 const creds = appEnv.getServiceCreds('cos-ippei');
@@ -31,9 +30,33 @@ const cos = new AWS.S3({
     apiKeyId: creds.apikey
 });
 
+// ファイルアップロードを設定する。
+const upload = multer({dest: path.join(__dirname, 'uploads')});
+
 // ミドルウェアを設定する。
 app.use(morgan('combined'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.post('/', upload.single('upload-file'), (req, res) => {
+    const uploadFile = fs.createReadStream(req.file.path);
+
+    cos.putObject({
+        Bucket: 'docs-ippei',
+        Key: req.file.originalname,
+        Body: uploadFile,
+        ContentType: req.file.mimetype
+    }, (error, data) => {
+        if (error) {
+            console.log('error', error);
+        } else {
+            console.log(data);
+        }
+        res.redirect('/');
+    });
+});
+
 
 app.get('/:key', (req, res) => {
     const key = req.params.key;
